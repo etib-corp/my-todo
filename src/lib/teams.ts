53f2aef47da1ab1bde8d8e9bdacd430a618cdebf
Nathan Maillot <nathan.maillot@epitech.eu>
2026-05-27 12:06:20 +0900

    feat: add teams back callbacks

---
+++ src/lib/teams.ts
@@ -0,0 +1,62 @@
+import { prisma } from "@/lib/prisma"
+
+interface TeamListingParams {
+  id?: number
+  name?: string
+}
+
+interface TeamCreationParams {
+  name: string
+  description: string
+}
+
+export async function listTeams(params: TeamListingParams) {
+  const where: any = {}
+
+  if (params.id !== undefined) {
+    where.id = params.id
+  }
+
+  if (params.name !== undefined) {
+    where.name = params.name
+  }
+
+  return prisma.team.findMany({ where, orderBy: { id: "asc" } })
+}
+
+export async function removeUserFromTeam(teamId: number, userId: number) {
+  return prisma.team.update({
+    where: { id: teamId },
+    data: {
+      users: {
+        disconnect: { id: userId },
+      },
+    },
+  })
+}
+
+export async function addUserToTeam(teamId: number, userId: number) {
+  return prisma.team.update({
+    where: { id: teamId },
+    data: {
+      users: {
+        connect: { id: userId },
+      },
+    },
+  })
+}
+
+export async function deleteTeam(teamId: number) {
+  return prisma.team.delete({
+    where: { id: teamId },
+  })
+}
+
+export async function createTeam(input: TeamCreationParams) {
+  return prisma.team.create({
+    data: {
+      name: input.name,
+      description: input.description,
+    },
+  })
+}
